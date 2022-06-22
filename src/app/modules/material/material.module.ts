8e18d61b43666185154892d5316a845465a2f0f2
rubentxogg <rubentxogg@hotmail.com>
2022-06-22 21:28:30 +0200

    [ADD] Header

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -5,6 +5,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
@@ -15,6 +16,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatIconModule,
     MatDividerModule,
     MatProgressSpinnerModule,
+    MatToolbarModule,
   ],
 })
 export class MaterialModule {}
