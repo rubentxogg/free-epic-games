e7bd8a5b0148ab9ac260731c5ee37a137d66a8ba
rubentxogg <rubentxogg@hotmail.com>
2022-07-10 21:18:28 +0200

    [ADD] environment prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  freeEpicGamesApiBaseUrl: 'https://free-epic-games.p.rapidapi.com/free',
+  XRapidAPIHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHeaderValue: 'free-epic-games.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '8deaa31b8dmsh5b87e23e302820fp172d2ajsn4050dd4c5bf7'
 };
